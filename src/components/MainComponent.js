6da9006b61e826d92fbb0aedfe7d95f1d62c02ca
istrelkov <istrelkov@kyriba.com>
2019-04-16 11:12:58 +0300

    Reacr router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
 
@@ -12,24 +14,25 @@ class Main extends Component {
 
         this.state = {
             dishes : DISHES,
-            selectedDish : null,
         };
     }
 
-    onDishSelect (dishId) {
-        this.setState({
-            selectedDish : dishId,
-        });
-    }
-
     render () {
+      const HomePage = () => {
+        return(
+          <Home />
+        );
+      }
+
         return (
             <div>
-                <Header/>
-                <Menu dishes={ this.state.dishes } onClick={ (dishId) => this.onDishSelect(dishId) }/>
-                <DishDetail
-                    selectedDish={ this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[ 0 ] }/>
-                <Footer/>
+              <Header />
+              <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" />
+              </Switch>
+              <Footer />
             </div>
         );
     }
