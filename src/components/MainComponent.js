29ee85b863a7483a02d481261de8ef48523dbea6
istrelkov <istrelkov@kyriba.com>
2019-04-25 17:23:26 +0300

    Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  }
+}
+
 
 class Main extends Component {
 
     constructor (props) {
         super(props);
-
-        this.state = {
-            dishes : DISHES,
-            comments : COMMENTS,
-            promotions : PROMOTIONS,
-            leaders : LEADERS,
-        };
     }
 
     render () {
@@ -31,7 +32,7 @@ class Main extends Component {
             promotions,
             leaders,
             comments
-        } = this.state;
+        } = this.props;
 
         const HomePage = () => {
             return (
@@ -69,4 +70,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
